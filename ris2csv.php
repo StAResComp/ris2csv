77fc36ffb89ea4fbe086d0eb5e8dbf00cb913d57
Swithun Crowe <cs2@st-andrews.ac.uk>
2018-02-21 17:15:45 +0000

    added some more columns

--- ris2csv.php
+++ ris2csv.php
@@ -1,8 +1,10 @@
 <?php
 
 // map RIS fields to CSV fields
-$map = array('A1' => 'Author', 'T1' => 'Title', 'JF' => 'Journal', 
-             'Y1' => 'Year', 'UR' => 'URL', 'N2' => 'Custom1');
+// keys longer than 2 letters are not RIS fields
+$map = array('A1' => 'Author', 'primary_author' => 'Primary author', 'presentation_authors' => 'Presentation authors', 'author_count' => 'Author count',
+             'T1' => 'Title', 'JF' => 'Journal', 
+             'Y1' => 'Year', 'DO' => 'DOI', 'N2' => 'Custom1', 'UR' => 'URL');
 
 // initialise record as hash of arrays
 function initialiseRecord() { //{{{
@@ -18,6 +20,71 @@ function initialiseRecord() { //{{{
 }
 //}}}
 
+// convert author names like Bloggs, Joe P into J. P. Bloggs
+function normaliseNames($names) { //{{{
+    $normalised = array();
+    $maxNames = 3;
+    
+    $n = 0;
+    foreach ($names as $i => $name) {
+        // skip names after third
+        if (++ $n > $maxNames) {
+            continue;
+        }
+        
+        // split name into surname and forenames and split up forenames
+        list ($surname, $forenames) = explode(', ', $name, 2);
+        $forenames = explode(' ', $forenames);
+        $fullname = '';
+        
+        // check that first letter of forename is uppercase
+        foreach ($forenames as $fn) {
+            $i = substr($fn, 0, 1);
+            // add to full name as initial
+            if (IntlChar::isupper($i)) {
+                $fullname .= sprintf('%s. ', $i);
+            }
+        }
+        
+        // add surname and add to normalised
+        $fullname .= $surname;
+        $normalised[] = $fullname;
+    }
+    
+    // join names
+    $presentation = implode(', ', $normalised);
+    
+    // check whether to add et al
+    if ($n > $maxNames) {
+        $presentation .= ' et al';
+    }
+    
+    return $presentation;
+}
+//}}}
+
+function normaliseJournal($journal) { //{{{
+    // words to remain lc
+    $lcWords = array('in', 'of', 'for', 'the', 'and');
+    
+    $words = explode(' ', $journal);
+    foreach ($words as $i => $w) {
+        // change & to and
+        if ('&' == $w) {
+            $words[$i] = 'and';
+        }
+        // word not in lc words
+        elseif (!in_array($w, $lcWords)) {
+            $words[$i] = substr_replace($w, IntlChar::toupper(substr($w, 0, 1)), 
+                                        0, 1);
+        }
+    }
+    
+    // return words joined by space
+    return implode(' ', $words);
+}
+//}}}
+
 // convert record into line for CSV file
 function writeRecord($record) { //{{{
     global $map;
@@ -28,23 +95,32 @@ function writeRecord($record) { //{{{
     foreach (array_keys($map) as $tag) {
         $value = $record[$tag];
         
-        // handle empty values here
-        if (!$value) {
+        // handle empty RIS values here
+        if (2 == strlen($tag) && !$value) {
             $arr[$tag] = '';
             continue;
         }
         
         switch ($tag) {
-            // comma delimit authors
+            // semi colon delimit authors
+            // and create primary author and presentation author fields
          case 'A1':
-            $arr[$tag] = implode(', ', $value);
+            $arr[$tag] = implode('; ', $value);
+            $arr['primary_author'] = $value[0];
+            $arr['presentation_authors'] = normaliseNames($value);
+            $arr['author_count'] = count($value);
             break;
             
-            // should only be 1 title/journal/abstract
+            // should only be 1 title/abstract
          case 'T1':
-         case 'JF':
          case 'N2':
+         case 'DO':
             $arr[$tag] = $value[0];
+            break;
+            
+            // normalise capitalisation of journal names
+         case 'JF':
+            $arr[$tag] = normaliseJournal($value[0]);
             break;
             
             // year - want first 4 digits
@@ -52,9 +128,11 @@ function writeRecord($record) { //{{{
             $arr[$tag] = substr($value[0], 0, 4);
             break;
             
-            // URL - space delimited
+            // URL - leave as array
          case 'UR':
-            $arr[$tag] = implode(' ', $value);
+            foreach ($value as $i => $url) {
+                $arr[sprintf('%s%d', $tag, $i)] = $url;
+            }
             break;
         }
     }
